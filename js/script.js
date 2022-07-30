95f468b454133757dfccc6b6dd23c14838a48ad9
Madlena C <madlena.calugareanu@gmail.com>
2022-07-30 14:11:17 +0300

    update html, css and js functions

--- js/script.js
+++ js/script.js
@@ -41,9 +41,42 @@ $( document ).ready(function() {
         })
     });
 
-    
+    $('#generalCulture').on('click', ()=>{
+        $('#answer').removeClass('d-none');
+        document.getElementById('numeJucator').innerHTML = getRandom(people);
+        document.getElementById('question').innerHTML = getRandomGeneralQuestion();
+        $('#next').on('click', ()=>{
+            document.getElementById('numeJucator').innerHTML = getRandom(people);
+            document.getElementById('question').innerHTML = getRandomGeneralQuestion();
+        })
+    });
+
 
     function getRandom(array){
         return array[Math.floor(Math.random() * array.length)];
     };
+
+    function getRandomGeneralQuestion(){
+        const generalQuestion = getRandom(generalCulture);
+        return generalQuestion[0];
+    }
+
+
+    
+
+    /* ------ Get Random Element, return it, remove it from array, then repeat 
+
+        function newRandomArray(array){
+            const newArray = Array.from(array);
+            const returnElement = getRandom(newArray);
+            const indexEl = newArray.indexOf(returnElement);
+            
+                newArray.splice(indexEl, 1);
+            
+            return returnElement;
+        }
+
+        console.log(newRandomArray(people));
+    */
+
 });
